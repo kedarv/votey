5aaabe15af899e77ee07ae1258e89687e4cdf8af
kedarv <kedar54@gmail.com>
2019-02-02 18:38:20 -0800

    Default configs from env

--- votey/config.py
+++ votey/config.py
@@ -1,3 +1,5 @@
+import os
+
 class Default:
-  SLACK_API_TOKEN = 'secret'
-  SQLALCHEMY_DATABASE_URI = 'yocalhost'
+  SLACK_API_TOKEN = os.environ.get('SLACK_API_TOKEN')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
