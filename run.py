dfe05304b1e7731571e3b09c2057835bd87fd46b
kedarv <kedar54@gmail.com>
2019-02-02 18:30:42 -0800

    add gunicorn support

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ app = create_app()
 with app.app_context():
 	db.create_all()
 
-app.run(host='0.0.0.0', port=5050, debug=True, threaded=True)
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5050, debug=True, threaded=True)
