8d5f82f4bacc3667389b15dafa1f04ec1f162cff
Matthew Ess <mess@yelp.com>
2019-02-08 01:36:57 -0800

    Reunindent

--- votey/__init__.py
+++ votey/__init__.py
@@ -10,34 +10,34 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app(config: Optional[Dict[str, Any]] = None) -> Flask:
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY='dev')
-    app.config.from_object('votey.config.Default')
+  # create and configure the app
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_mapping(SECRET_KEY='dev')
+  app.config.from_object('votey.config.Default')
 
-    if config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_envvar('VOTEY_CONFIG', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(config)
+  if config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_envvar('VOTEY_CONFIG', silent=True)
+  else:
+    # load the test config if passed in
+    app.config.from_mapping(config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+  # ensure the instance folder exists
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
 
-    db.init_app(app)
+  db.init_app(app)
 
-    # paasta healthcheck
-    @app.route('/status')  # type: ignore
-    def dummy() -> str:
-        hostname = app.config.get('SERVER_NAME') or gethostname()
-        return f'OK - {hostname}'
+  # paasta healthcheck
+  @app.route('/status')  # type: ignore
+  def dummy() -> str:
+    hostname = app.config.get('SERVER_NAME') or gethostname()
+    return f'OK - {hostname}'
 
-    # slack interaction
-    from . import slack
-    app.register_blueprint(slack.bp)
+  # slack interaction
+  from . import slack
+  app.register_blueprint(slack.bp)
 
-    return app
+  return app
--- votey/slack.py
+++ votey/slack.py
@@ -21,228 +21,228 @@ from votey import db
 bp = Blueprint('slack', __name__)
 
 NUM_TO_SLACKMOJI = {
-    1: ':one:',
-    2: ':two:',
-    3: ':three:',
-    4: ':four:',
-    5: ':five:',
-    6: ':six:',
-    7: ':seven:',
+  1: ':one:',
+  2: ':two:',
+  3: ':three:',
+  4: ':four:',
+  5: ':five:',
+  6: ':six:',
+  7: ':seven:',
 }
 
 JSON = Dict[str, str]
 
 @bp.route('/slack', methods=['POST'])  # type: ignore
 def slack() -> str:
-    return handle_button_interaction(request.form) \
-        if valid_request(request) and request.form.get('payload') \
-        else handle_poll_creation(request.form) \
-        if valid_request(request) \
-        else ''
+  return handle_button_interaction(request.form) \
+    if valid_request(request) and request.form.get('payload') \
+    else handle_poll_creation(request.form) \
+    if valid_request(request) \
+    else ''
 
 @bp.route('/oauth', methods=['GET'])  # type: ignore
 def oauth() -> str:
-    code = request.args.get('code')
-    client_id = current_app.config['CLIENT_ID']
-    client_secret = current_app.config['CLIENT_SECRET']
-    oauth = requests.get('https://slack.com/api/oauth.access', params={
-        'code': code,
-        'client_id': client_id,
-        'client_secret': client_secret,
-    })
-    if oauth.json().get('ok'):
-        team_id = oauth.json().get('team_id')
-        token = oauth.json().get('access_token')
-        name = oauth.json().get('team_name')
-        workspace = Workspace.query.filter_by(team_id=team_id).first()
-        if workspace is not None:
-            workspace.token = token
-        else:
-            workspace = Workspace(team_id, name, token)
-            db.session.add(workspace)
-        db.session.commit()
-        return f'thanks! votey has been installed to <b>{name}</b>. you can close this tab.'
-    return 'something went wrong :('
+  code = request.args.get('code')
+  client_id = current_app.config['CLIENT_ID']
+  client_secret = current_app.config['CLIENT_SECRET']
+  oauth = requests.get('https://slack.com/api/oauth.access', params={
+    'code': code,
+    'client_id': client_id,
+    'client_secret': client_secret,
+  })
+  if oauth.json().get('ok'):
+    team_id = oauth.json().get('team_id')
+    token = oauth.json().get('access_token')
+    name = oauth.json().get('team_name')
+    workspace = Workspace.query.filter_by(team_id=team_id).first()
+    if workspace is not None:
+      workspace.token = token
+    else:
+      workspace = Workspace(team_id, name, token)
+      db.session.add(workspace)
+    db.session.commit()
+    return f'thanks! votey has been installed to <b>{name}</b>. you can close this tab.'
+  return 'something went wrong :('
 
 def handle_poll_creation(req: JSON) -> str:
-    workspace = Workspace.query.filter_by(team_id=req.get('team_id')).first()
-    command, anonymous = get_command_from_req(req, workspace)
-    if command is None:
-        return ''
+  workspace = Workspace.query.filter_by(team_id=req.get('team_id')).first()
+  command, anonymous = get_command_from_req(req, workspace)
+  if command is None:
+    return ''
 
-    poll_question = command.pop(0)
-    actions = []
-    fields = []
+  poll_question = command.pop(0)
+  actions = []
+  fields = []
 
-    poll = Poll(uuid.uuid4(), poll_question, anonymous)
-    db.session.add(poll)
+  poll = Poll(uuid.uuid4(), poll_question, anonymous)
+  db.session.add(poll)
+  db.session.commit()
+
+  for counter, option_text in enumerate(command):
+    option = Option(poll.id, option_text)
+    db.session.add(option)
     db.session.commit()
 
-    for counter, option_text in enumerate(command):
-        option = Option(poll.id, option_text)
-        db.session.add(option)
-        db.session.commit()
+    actions.append({
+      'name': 'vote',
+      'text': NUM_TO_SLACKMOJI[counter + 1],
+      'value': option.id,
+      'type': 'button'
+    })
+    fields.append({
+      'title': '',
+      'value': f'{NUM_TO_SLACKMOJI[counter + 1]} {option_text}\n\n\n',
+      'short': False,
+      'mrkdwn': 'true',
+    })
 
-        actions.append({
-            'name': 'vote',
-            'text': NUM_TO_SLACKMOJI[counter + 1],
-            'value': option.id,
-            'type': 'button'
-        })
-        fields.append({
-            'title': '',
-            'value': f'{NUM_TO_SLACKMOJI[counter + 1]} {option_text}\n\n\n',
-            'short': False,
-            'mrkdwn': 'true',
-        })
+  attachments = [
+    {
+      'text': poll_question,
+      'mrkdwn_in': ['fields'],
+      'color': '#6ecadc',
+      'fields': fields,
+      'footer': f'Anonymous poll created by <@{req.get("user_id")}>'
+      if anonymous else f'Poll created by <@{req.get("user_id")}>',
+      'ts': time.time(),
+    },
+    {
+      'text': ' ',
+      'callback_id': poll.poll_identifier(),
+      'attachment_type': 'default',
+      'color': '#6ecadc',
+      'actions': actions
+    }
+  ]
 
-    attachments = [
-        {
-            'text': poll_question,
-            'mrkdwn_in': ['fields'],
-            'color': '#6ecadc',
-            'fields': fields,
-            'footer': f'Anonymous poll created by <@{req.get("user_id")}>'
-            if anonymous else f'Poll created by <@{req.get("user_id")}>',
-            'ts': time.time(),
-        },
-        {
-            'text': ' ',
-            'callback_id': poll.poll_identifier(),
-            'attachment_type': 'default',
-            'color': '#6ecadc',
-            'actions': actions
-        }
-    ]
-
-    requests.post(
-        'https://slack.com/api/chat.postMessage',
-        json = {'channel': req.get('channel_id'), 'attachments': attachments},
-        headers={'Authorization': f'Bearer {workspace.token}'},
-    )
-    return ''
+  requests.post(
+    'https://slack.com/api/chat.postMessage',
+    json = {'channel': req.get('channel_id'), 'attachments': attachments},
+    headers={'Authorization': f'Bearer {workspace.token}'},
+  )
+  return ''
 
 def handle_button_interaction(req: JSON) -> str:
-    response = json.loads(req.get('payload', ''))
-    poll = Poll.query.filter_by(
-        identifier=response.get('callback_id')
+  response = json.loads(req.get('payload', ''))
+  poll = Poll.query.filter_by(
+    identifier=response.get('callback_id')
+  ).first()
+  option = Option.query.filter_by(
+    id=response.get('actions')[0]['value']
+  ).first()
+  workspace = Workspace.query.filter_by(
+    team_id=response.get('team').get('id')
+  ).first()
+  user = response.get('user').get('id')
+  channel = response.get('channel').get('id')
+  original_message = response.get('original_message')
+  attachments = original_message.get('attachments')
+
+  if poll is not None and option is not None:
+    vote = Vote.query.filter_by(
+      poll_id=poll.id,
+      option_id=option.id,
+      user=user
     ).first()
-    option = Option.query.filter_by(
-        id=response.get('actions')[0]['value']
-    ).first()
-    workspace = Workspace.query.filter_by(
-        team_id=response.get('team').get('id')
-    ).first()
-    user = response.get('user').get('id')
-    channel = response.get('channel').get('id')
-    original_message = response.get('original_message')
-    attachments = original_message.get('attachments')
 
-    if poll is not None and option is not None:
-        vote = Vote.query.filter_by(
-            poll_id=poll.id,
-            option_id=option.id,
-            user=user
-        ).first()
+    if vote:
+      db.session.delete(vote)
+    else:
+      vote = Vote(poll.id, option.id, user)
+      db.session.add(vote)
+    db.session.commit()
 
-        if vote:
-            db.session.delete(vote)
-        else:
-            vote = Vote(poll.id, option.id, user)
-            db.session.add(vote)
-        db.session.commit()
+    position = 0
+    for button in attachments[1].get('actions'):
+      if int(button.get('value')) == option.id:
+        position = int(button.get('id'))
 
-        position = 0
-        for button in attachments[1].get('actions'):
-            if int(button.get('value')) == option.id:
-                position = int(button.get('id'))
-
-        votes = Vote.query.filter_by(option_id=option.id).all()
-        num = f'`{len(votes)}`'
-        field_text = f'{NUM_TO_SLACKMOJI[position]} {option.option_text}\t' \
-                                 f'{num if votes else ""}\n' \
-                                 f'{thumbs(votes) if poll.anonymous else names(votes)}\n\n'
-        attachments[0].get('fields')[position-1]['value'] = field_text
-        requests.post('https://slack.com/api/chat.update', json = {
-            'channel': channel,
-            'ts': response.get('message_ts'),
-            'text': '',
-            'attachments': attachments,
-        },  headers={'Authorization': f'Bearer {workspace.token}'})
-    return ''
+    votes = Vote.query.filter_by(option_id=option.id).all()
+    num = f'`{len(votes)}`'
+    field_text = f'{NUM_TO_SLACKMOJI[position]} {option.option_text}\t' \
+                 f'{num if votes else ""}\n' \
+                 f'{thumbs(votes) if poll.anonymous else names(votes)}\n\n'
+    attachments[0].get('fields')[position-1]['value'] = field_text
+    requests.post('https://slack.com/api/chat.update', json = {
+      'channel': channel,
+      'ts': response.get('message_ts'),
+      'text': '',
+      'attachments': attachments,
+    },  headers={'Authorization': f'Bearer {workspace.token}'})
+  return ''
 
 
 def thumbs(votes: List[Vote]) -> str:
-    return ':thumbsup:' * len(votes)
+  return ':thumbsup:' * len(votes)
 
 
 def names(votes: List[Vote]) -> str:
-    return ','.join([f'<@{vote.user}>' for vote in votes])
+  return ','.join([f'<@{vote.user}>' for vote in votes])
 
 
 def valid_request(request: LocalProxy) -> bool:
-    timestamp = request.headers['X-Slack-Request-Timestamp']
-    slack_signature = request.headers['X-Slack-Signature']
-    # Avoid replay attacks
-    if abs(time.time() - int(timestamp)) > (60*5):
-        return False
-    sig_basestring = str.encode(f'v0:{timestamp}:') + request.get_data()
-    request_hash = 'v0=' + hmac.new(
-            bytes(current_app.config['SIGNING_SECRET'], encoding='utf-8'),
-            sig_basestring,
-            hashlib.sha256,
-    ).hexdigest()
-    return hmac.compare_digest(request_hash, slack_signature)
+  timestamp = request.headers['X-Slack-Request-Timestamp']
+  slack_signature = request.headers['X-Slack-Signature']
+  # Avoid replay attacks
+  if abs(time.time() - int(timestamp)) > (60*5):
+    return False
+  sig_basestring = str.encode(f'v0:{timestamp}:') + request.get_data()
+  request_hash = 'v0=' + hmac.new(
+      bytes(current_app.config['SIGNING_SECRET'], encoding='utf-8'),
+      sig_basestring,
+      hashlib.sha256,
+  ).hexdigest()
+  return hmac.compare_digest(request_hash, slack_signature)
 
 def get_command_from_req(
-    request: JSON,
-    workspace: Workspace
+  request: JSON,
+  workspace: Workspace
 ) -> Tuple[Optional[List[str]], bool]:
-    try:
-        split = shlex.split(request.get('text', '') \
-            .replace('“','"') \
-            .replace('”','"'))
-    except ValueError as e:
-        send_ephemeral_message(
-            workspace,
-            request.get('channel_id', ''),
-            request.get('user_id', ''),
-            f'We had trouble parsing that - {e}',
-        )
-        return None, False
-    if len(split) < 3:
-        send_ephemeral_message(
-            workspace,
-            request.get('channel_id', ''),
-            request.get('user_id', ''),
-            'Oops - a poll needs to have at least two options. ' \
-            'Try again with `/votey "question" "option 1" "option 2"`',
-        )
-        return None, False
-    if len(split) > 7 or (not is_anonymous(split) and len(split) > 6):
-        send_ephemeral_message(
-            workspace,
-            request.get('channel_id', ''),
-            request.get('user_id', ''),
-            'Sorry - Votey only supports 5 options at the moment, ' \
-            'due to limitations in the Slack API',
-        )
-        return None, False
-    if is_anonymous(split):
-        return split[0:-1], True
-    return split, False
+  try:
+    split = shlex.split(request.get('text', '') \
+      .replace('“','"') \
+      .replace('”','"'))
+  except ValueError as e:
+    send_ephemeral_message(
+      workspace,
+      request.get('channel_id', ''),
+      request.get('user_id', ''),
+      f'We had trouble parsing that - {e}',
+    )
+    return None, False
+  if len(split) < 3:
+    send_ephemeral_message(
+      workspace,
+      request.get('channel_id', ''),
+      request.get('user_id', ''),
+      'Oops - a poll needs to have at least two options. ' \
+      'Try again with `/votey "question" "option 1" "option 2"`',
+    )
+    return None, False
+  if len(split) > 7 or (not is_anonymous(split) and len(split) > 6):
+    send_ephemeral_message(
+      workspace,
+      request.get('channel_id', ''),
+      request.get('user_id', ''),
+      'Sorry - Votey only supports 5 options at the moment, ' \
+      'due to limitations in the Slack API',
+    )
+    return None, False
+  if is_anonymous(split):
+    return split[0:-1], True
+  return split, False
 
 def is_anonymous(words: List[str]) -> bool:
-    return words[-1] == 'anonymous'
+  return words[-1] == 'anonymous'
 
 def send_ephemeral_message(
-    workspace: Workspace,
-    channel: str,
-    user: str,
-    text: str
+  workspace: Workspace,
+  channel: str,
+  user: str,
+  text: str
 ) -> requests.Response:
-    return requests.post(
-        'https://slack.com/api/chat.postEphemeral',
-        json = {'channel': channel, 'user': user, 'text': text},
-        headers={'Authorization': f'Bearer {workspace.token}'},
-    )
+  return requests.post(
+    'https://slack.com/api/chat.postEphemeral',
+    json = {'channel': channel, 'user': user, 'text': text},
+    headers={'Authorization': f'Bearer {workspace.token}'},
+  )
